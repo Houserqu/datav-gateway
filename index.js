6a9e58c244520620e5db7dca031125adb7b7b85e
houser <houserqu@qq.com>
2019-04-16 11:49:13 +0800

    feat: not check login

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express")
 const httpProxy = require("express-http-proxy")
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const checkLogin = require('./middleware/checkLogin')
-const cors = require('cors')
+const session = require("express-session")
+const cookieParser = require("cookie-parser")
+const checkLogin = require("./middleware/checkLogin")
+const cors = require("cors")
 
 // user 用户相关
 const loginServiceProxy = httpProxy("http://127.0.0.1:7002", {
-  userResDecorator: function (proxyRes, proxyResData, userReq, userRes) {
+  userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
     // 判断 user 代理服务返回结果，如果登录成功，写入 userID 到网关 session
 
-    const dataStr = proxyResData.toString('utf8').replace(/\0/g, "")
-    const data = JSON.parse(dataStr);
+    const dataStr = proxyResData.toString("utf8").replace(/\0/g, "")
+    const data = JSON.parse(dataStr)
     if (data.success) {
-      console.log('登录成功');
-      userReq.session.userId = data.data.id;
+      console.log("登录成功")
+      userReq.session.userId = data.data.id
     }
     return data
   }
@@ -32,38 +32,48 @@ const appServiceProxy = httpProxy("http://127.0.0.1:7004")
 // static 静态文件
 const staticServiceProxy = httpProxy("http://127.0.0.1:7005")
 
+// 公共接口
+const pubServiceProxy = httpProxy("http://127.0.0.1:7006")
 
 const app = express()
 
-app.use(cors({
-  origin: 'http://localhost:8000',
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: "http://localhost:8000",
+    credentials: true
+  })
+)
 
 app.use(cookieParser())
 
-app.use(session({
-  secret: 'datav',//与cookieParser中的一致
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "datav", //与cookieParser中的一致
+    resave: true,
+    saveUninitialized: true
+  })
+)
 
 // 写入 session 中的 userid 信息到 header 中，用于其他服务获取用户信息
 app.use((req, res, next) => {
-  req.headers['certificate-userid'] = req.session.userId || '';
+  req.headers["certificate-userid"] = req.session.userId || ""
   next()
 })
 
+/**
+ * 公共接口代理
+ */
+// app.all("/api/app/list", httpProxy("http://127.0.0.1:7004/"))
+
 app.all("/api/login", (req, res, next) => {
-  console.log(req.session);
   loginServiceProxy(req, res, next)
 })
 
 app.get("/api/logout", (req, res, next) => {
-  req.session.regenerate(function (err) {
+  req.session.regenerate(function(err) {
     // will have a new session here
     if (!err) {
-      res.send({ success: true, msg: '注销成功', data: null })
+      res.send({ success: true, msg: "注销成功", data: null })
     }
   })
 })
@@ -72,7 +82,11 @@ app.all("/static/*", (req, res, next) => {
   staticServiceProxy(req, res, next)
 })
 
-app.all("/api/app/*", checkLogin, (req, res, next) => {
+app.all("/api/app/*", (req, res, next) => {
+  appServiceProxy(req, res, next)
+})
+
+app.all("/api/source/*", checkLogin, (req, res, next) => {
   appServiceProxy(req, res, next)
 })
 
@@ -84,6 +98,6 @@ app.all("/api/auth/*", checkLogin, (req, res, next) => {
   authServiceProxy(req, res, next)
 })
 
-app.listen(7001, function () {
-  console.log('Example app listening on port 7001!');
-});
+app.listen(7001, function() {
+  console.log("Example app listening on port 7001!")
+})
